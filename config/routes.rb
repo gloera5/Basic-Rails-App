d3c27b537a52646b25ac44ae04eed16dd08c1e27
gloera5 <gloera5@live.com>
2015-05-29 23:23:00 +0000

    Updated Master

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  resources :posts
+resources :posts, :advertisement
   
-  get 'about' => 'welcome#about'
+  
 
   get 'welcome/contact'
-=======
-  resources :posts, :advertisement
+
+
   
    get 'about' => 'welcome#about'
->>>>>>> Assignment-27-CRUD
+
 
    root to: 'welcome#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150504004020) do
-=======
-=======
->>>>>>> checkpoint-26-SeedData
-ActiveRecord::Schema.define(version: 20150517211752) do
-=======
 ActiveRecord::Schema.define(version: 20150518064512) do
 
   create_table "advertisements", force: true do |t|
@@ -29,7 +20,6 @@ ActiveRecord::Schema.define(version: 20150518064512) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
->>>>>>> Assignment-27-CRUD
 
   create_table "answers", force: true do |t|
     t.text     "body"
@@ -39,13 +29,6 @@ ActiveRecord::Schema.define(version: 20150518064512) do
   end
 
   add_index "answers", ["Question_id"], name: "index_answers_on_Question_id"
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 4b67b34ff3ac57d897c10df8dd61afecfc4dfbb1
-=======
->>>>>>> checkpoint-26-SeedData
-=======
->>>>>>> Assignment-27-CRUD
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -57,32 +40,12 @@ ActiveRecord::Schema.define(version: 20150518064512) do
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "posts", force: true do |t|
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    t.string   "tite"
-=======
     t.string   "title"
->>>>>>> 4b67b34ff3ac57d897c10df8dd61afecfc4dfbb1
-=======
-    t.string   "title"
->>>>>>> checkpoint-26-SeedData
-=======
-    t.string   "title"
->>>>>>> Assignment-27-CRUD
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> checkpoint-26-SeedData
-=======
->>>>>>> Assignment-27-CRUD
   create_table "questions", force: true do |t|
     t.string   "title"
     t.boolean  "resolved"
@@ -90,11 +53,4 @@ ActiveRecord::Schema.define(version: 20150518064512) do
     t.datetime "updated_at"
   end
 
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 4b67b34ff3ac57d897c10df8dd61afecfc4dfbb1
-=======
->>>>>>> checkpoint-26-SeedData
-=======
->>>>>>> Assignment-27-CRUD
 end
