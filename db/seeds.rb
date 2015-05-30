bfd41f8e8b742e21baa2c14e9a8ebb2be224d6a3
gloera5 <gloera5@live.com>
2015-05-30 22:53:04 +0000

    File fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,13 @@
  # Create Posts
  50.times do
    Post.create!(
-<<<<<<< HEAD
+
      title:  Faker::Lorem.sentence + " A New Title",
      body:   Faker::Lorem.paragraph + " A new sentence."
-=======
+
      title:  Faker::Lorem.sentence,
      body:   Faker::Lorem.paragraph
->>>>>>> checkpoint-26-SeedData
+
    )
  end
  posts = Post.all
@@ -18,11 +18,10 @@
  100.times do
    Comment.create!(
      post: posts.sample,
-<<<<<<< HEAD
+
      body: Faker::Lorem.paragraph + " A new comment."
-=======
      body: Faker::Lorem.paragraph
->>>>>>> checkpoint-26-SeedData
+
    )
  end
  
