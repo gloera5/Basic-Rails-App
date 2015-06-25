cd0911f93f1135af4980c88387fbf696d9904b4b
gloera5 <gloera5@live.com>
2015-06-25 02:04:40 +0000

    Assignment 39 Completed

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,33 @@
 class PostPolicy < ApplicationPolicy
+  class Scope
+    attr_reader :user, :scope
+
   def index?
     true
   end
+  def index
+    @posts = PostPolicy::Scope.new(current_user, Post).resolve
+  end
+  
+  def new?
+    true
+  end
+ 
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        scope.where(:published => true)
+      end
+    end
+    
+  def update?
+    true
+  end
+end
 end
