b544db248fbdf7a8c7d27110940d352cd862905d
gloera5 <gloera5@live.com>
2015-07-12 00:02:02 +0000

    Assignment 45 Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@
  
 
 # Create Topics
- 15.times do
+ 100.times do
    Topic.create!(
      name:         Faker::Lorem.sentence,
      description:  Faker::Lorem.paragraph
@@ -23,7 +23,7 @@
  topics = Topic.all
  
  # Create Posts
- 50.times do
+ 100.times do
    Post.create!(
      user:   users.sample,
      topic:  topics.sample,
