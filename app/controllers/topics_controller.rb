6654a2d4dfe76cf82d304d99d0514edc2164f09a
gloera5 <gloera5@live.com>
2015-07-17 00:53:14 +0000

    Checkpoint 47 in progress

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -46,4 +46,17 @@ class TopicsController < ApplicationController
    def topic_params
      params.require(:topic).permit(:name, :description, :public)
    end
+    
+   def destroy
+     @topic = Topic.find(params[:id])
+ 
+     authorize @topic
+     if @topic.destroy
+       flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
+       redirect_to topics_path
+     else
+       flash[:error] = "There was an error deleting the topic."
+       render :show
+     end
+   end
 end
