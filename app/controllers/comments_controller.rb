bf6f87fc6de54c6819abad104d4d5fb8badc15db
gloera5 <gloera5@live.com>
2015-07-29 04:07:12 +0000

    Checkpoint in progress

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,19 @@ class CommentsController < ApplicationController
   def create
     @comment = current_user.commets.build(params.require(:commnet).permit(:body))
   end
+  
+  def destroy
+     @topic = Topic.find(params[:topic_id])
+     @post = @topic.posts.find(params[:post_id])
+     @comment = @post.comments.find(params[:id])
+ 
+     authorize @comment
+     if @comment.destroy
+       flash[:notice] = "Comment was removed."
+       redirect_to [@topic, @post]
+     else
+       flash[:error] = "Comment couldn't be deleted. Try again."
+       redirect_to [@topic, @post]
+     end
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts, :welcome, :topics, :comments
    resources :topics do
-     resources :posts, except: [:index]
-   end
-   resources :coments do
-     resources :posts, only: [:create]
-   end
+     resources :posts, except: [:index] do
+      resources :comments, only: [:create, :destroy]
+    end
+   
 
 
   
