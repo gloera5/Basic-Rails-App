a3dc63a3ead139bc4bd73d73c37d5f270c264072
gloera5 <gloera5@live.com>
2015-07-30 00:12:25 +0000

    comments assignment in progress

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -2,4 +2,10 @@ class CommentPolicy < ApplicationPolicy
   def destroy?
     user.present? && (record.user == user || user.admin? || user.moderator?)
   end
+  def new?
+    user.present? && (record.user == user || user.admin? || user.moderator?)
+  end
+  def create?
+    user.present? && (record.user == user || user.admin? || user.moderator?)
+  end
 end
