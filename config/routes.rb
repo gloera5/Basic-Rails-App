ce6c2486bef17253c50568ed2a2e9ce28e47686c
gloera5 <gloera5@live.com>
2015-09-10 07:13:20 +0000

    Checkpoint 55 complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,17 @@ Rails.application.routes.draw do
   devise_for :users
   resources :welcome, :topics
    resources :topics do
-     resources :posts, only: [:index, :new, :create]
+     resources :posts, except: [:index], controller: 'topics/post'
    end
 
-   resources :posts, except: [:index] do
+   resources :posts, only: [:index] do
      resources :comments, only: [:create]
      resources :favorites, only: [:create, :destroy]
    post '/up-vote' => 'votes#up_vote', as: :up_vote
    post '/down-vote' => 'votes#down_vote', as: :down_vote
   end
    
-  resource :user, only: [:edit, :update, :show] do
+  resources :users, only: [:edit, :update, :show, :index] do
     collection do
     patch 'update_password'
   end
