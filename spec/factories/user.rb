69ae79f033f9028ddca19faebdded1b13ad4930b
gloera5 <gloera5@live.com>
2015-09-19 22:10:15 +0000

    Assignmnet-56-Complete

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -6,4 +6,14 @@ FactoryGirl.define do
      password_confirmation "helloworld"
      confirmed_at Time.now
    end
- end
+  
+   factory :user_with_post_and_comment, parent: :user do
+     association :title, factory: :post
+     association :body, factory: :post
+     association :topic, factory: :post
+     association :body, factory: :comment
+     
+     after(:build) { |post| post.class.skip_callback(:create) }
+     after(:build) { |comment| comment.class.skip_callback(:create) }
+   end
+end
